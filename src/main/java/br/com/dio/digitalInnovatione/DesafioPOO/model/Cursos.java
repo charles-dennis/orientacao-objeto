e215bbdd04f454b7079ef7019568eea71389ed29
Charles Dennis <charlesdennis2009@hotmail.com>
2022-01-06 10:46:03 -0300

    Criacao do desafio

---
+++ src/main/java/br/com/dio/digitalInnovatione/DesafioPOO/model/Cursos.java
@@ -0,0 +1,5 @@
+package br.com.dio.digitalInnovatione.DesafioPOO.model;
+
+public class Cursos {
+
+}
---
+++ src/test/java/br/com/dio/digitalInnovatione/DesafioPOO/DesafioPooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.digitalInnovatione.DesafioPOO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
